906b5d10b465b6d2276d519fbce9762ce0d3a0ca
Aleks-NP300 <aleks.ce267@gmail.com>
2017-05-04 13:03:25 +0300

    RecyclerView implemented

--- app/src/main/java/com/kuzko/aleksey/guessword/LoginActivity.java
+++ app/src/main/java/com/kuzko/aleksey/guessword/LoginActivity.java
@@ -3,6 +3,7 @@ package com.kuzko.aleksey.guessword;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,8 +23,9 @@ public class LoginActivity extends AppCompatActivity {
     private TextView userLogin, userPassword;
     private Button signInButton;
     public final static String USERNAME_KEY = "username";
+    private User loggedUser;
     GuesswordService guesswordService;
-    List<User> users;
+//    List<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,44 +34,47 @@ public class LoginActivity extends AppCompatActivity {
         userLogin = (TextView) findViewById(R.id.userLogin);
         userPassword = (TextView) findViewById(R.id.userPassword);
         signInButton = (Button) findViewById(R.id.signInButton);
-        signInButton.setClickable(false);
         guesswordService = GuesswordRepository.getInstance().getGuesswordService();
 
-        guesswordService.fetchAllUsers()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        users -> {
-                            this.users = users;
-                            signInButton.setClickable(true);
-                        },
-                        throwable -> Toast.makeText(LoginActivity.this, "LoginActivity " + throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show()
-                );
+
     }
 
-    private User findUser(String givenUserLogin){
-        if(this.users != null){
-            for(User currentUser : this.users){
+    private boolean logIn(String givenUserLogin, String givenUserPassword, List<User> existedUsers){
+        if(existedUsers != null){
+            for(User currentUser : existedUsers){
                 if(currentUser.getLogin().equals(givenUserLogin)){
-                    return currentUser;
+                    if(currentUser.getPassword().equals(givenUserPassword)){
+                        loggedUser = currentUser;
+                        return true;
+                    }
                 }
             }
         }
-        return null;
+        return false;
     }
 
     public void signInButtonClicked(View view) {
         String enteredUserLogin = userLogin.getText().toString();
         String enteredUserPassword = userPassword.getText().toString();
-        User foundUser = findUser(enteredUserLogin);
-        if(foundUser == null){
-            userLogin.setError("No users with such name were found");
-        }else if(!foundUser.getPassword().equals(enteredUserPassword)){
-            userPassword.setError("Invalid password");
-        }else {
-            Intent toLearnActivityIntent = new Intent(this, LearnActivity.class);
-            toLearnActivityIntent.putExtra(USERNAME_KEY, foundUser.getId());
-            startActivity(toLearnActivityIntent);
-        }
+        guesswordService.fetchAllUsers()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        response -> {
+                            Log.d("INFO", response.raw().request().url().toString());
+                            if(logIn(enteredUserLogin, enteredUserPassword, response.body())){
+                                Intent toLearnActivityIntent = new Intent(this, LearnActivity.class);
+                                toLearnActivityIntent.putExtra(USERNAME_KEY, loggedUser.getId());
+                                startActivity(toLearnActivityIntent);
+                            }else {
+                                userLogin.setError(getString(R.string.string_incorrect_login_password));
+                                userPassword.setError(getString(R.string.string_incorrect_login_password));
+                            }
+                        },
+                        throwable -> {
+                            Toast.makeText(LoginActivity.this, "LoginActivity " + throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                            Log.d("ERROR",  "LoginActivity " + throwable.getLocalizedMessage());
+                        }
+                );
     }
 }
